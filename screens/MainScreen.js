d7ed7b13f8f663bc76d0bf5192636f23ed3be1d6
Jesse Ruder <jesse@getexponent.com>
2017-06-23 16:08:00 -0700

    improve

--- screens/MainScreen.js
+++ screens/MainScreen.js
@@ -17,8 +17,56 @@ const MAX_RATE = 1.0;
 const RATE_CHANGE_SPEED = 0.003;
 const IDLE_RATE_DECREASE = 0.002;
 const MIN_IDLE_RATE = 0.3;
+const DONT_STOP_TEXT = `DON'T STOP!!!`;
 
-export default class HomeScreen extends React.Component {
+class RobotText extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      currentNumCharacters: 0,
+      showingBlinker: true,
+    };
+  }
+
+  componentWillMount() {
+    setInterval(this._writeCharacter, 60);
+    setInterval(this._flashBlinker, 300);
+  }
+
+  componentWillReceiveProps(nextProps) {
+    if (this.props.text !== nextProps.text) {
+      this.setState({
+        currentNumCharacters: 0,
+      })
+    }
+  }
+
+  _writeCharacter = () => {
+    this.setState({
+      currentNumCharacters: this.state.currentNumCharacters + 1,
+    });
+  };
+
+  _flashBlinker = () => {
+    let showingBlinker = !this.state.showingBlinker;
+    if (this.state.currentNumCharacters >= this.props.text.length - 1) {
+      showingBlinker = false;
+    }
+
+    this.setState({
+      showingBlinker,
+    });
+  }
+
+  render() {
+    return (
+      <Text style={styles.text}>{this.props.text.substring(0, this.state.currentNumCharacters) + (this.state.showingBlinker ? '_' : '')}</Text>
+    );
+  }
+}
+
+export default class MainScreen extends React.Component {
   static navigationOptions = {
     header: null,
   };
@@ -32,22 +80,25 @@ export default class HomeScreen extends React.Component {
       ratio: 0,
       numUpdates: 0,
       timeStartUpdates: null,
+      text: '',
     };
   }
 
   componentDidMount() {
-    //this._checkPedometerAsync();
+    this._checkPedometerAsync();
   }
 
   _checkPedometerAsync = async () => {
     let isPedometerAvailable = Pedometer.isAvailableAsync();
-    this.setState({
-      isPedometerAvailable,
-      lastTimeUpdated: Date.now(),
-      timeStartUpdates: Date.now(),
-    });
 
     if (isPedometerAvailable) {
+      this.setState({
+        text: 'Start running to play music',
+        isPedometerAvailable,
+        lastTimeUpdated: Date.now(),
+        timeStartUpdates: Date.now(),
+      });
+
       await this._loadMusicAsync();
       Pedometer.watchStepCount(this._onStepAsync);
       setInterval(this._updateRateAsync, 100);
@@ -78,7 +129,12 @@ export default class HomeScreen extends React.Component {
       await this._soundObject.setIsLoopingAsync(true);
       this._isSoundPlaying = true;
       stateObject.currentRate = rate;
+      stateObject.text = 'Nice! Keep going'
       this._soundObject.setRateAsync(rate, false);
+    }
+
+    if (this.state.text === DONT_STOP_TEXT) {
+      stateObject.text = 'NICE!!';
     }
 
     this.setState({
@@ -94,6 +150,7 @@ export default class HomeScreen extends React.Component {
 
   _updateRateAsync = async () => {
     let { currentRate, desiredRate, lastTimeUpdated, avgTimeBetweenUpdates } = this.state;
+    let oldCurrentRate = currentRate;
     if (currentRate < desiredRate) {
       currentRate += RATE_CHANGE_SPEED;
       if (currentRate > desiredRate) {
@@ -108,15 +165,30 @@ export default class HomeScreen extends React.Component {
       }
     }
 
+    let stateObject = {};
+    if (oldCurrentRate >= MIN_RATE + 0.1 && currentRate < MIN_RATE + 0.1) {
+      stateObject.text = "Faster!";
+    } else if (oldCurrentRate <= MAX_RATE - 0.2 && currentRate > MAX_RATE - 0.2) {
+      stateObject.text = "Keep going!"
+    } else if (oldCurrentRate <= MAX_RATE - 0.1 && currentRate > MAX_RATE - 0.1) {
+      stateObject.text = "YOU'RE FAST!!"
+    }
+
     let timeBetweenUpdates = Date.now() - lastTimeUpdated;
-    if (timeBetweenUpdates > avgTimeBetweenUpdates) {
+    if (timeBetweenUpdates > avgTimeBetweenUpdates * 1.2) {
       desiredRate -= IDLE_RATE_DECREASE;
+
+      stateObject.text = DONT_STOP_TEXT;
+
       if (desiredRate < MIN_IDLE_RATE) {
         desiredRate = MIN_IDLE_RATE;
+
+        stateObject.text = 'BOOOOOOOO';
       }
     }
 
     this.setState({
+      ...stateObject,
       currentRate,
       desiredRate,
     });
@@ -139,15 +211,16 @@ export default class HomeScreen extends React.Component {
         <Image source={require('../assets/images/pavement-texture.jpg')} style={styles.backgroundImage} />
         <View style={styles.insideContainer}>
           <Image source={require('../assets/images/logo.png')} style={styles.logo} />
-          <Text style={styles.text}>Start moving to hear the music</Text>
-          <TouchableOpacity onPress={() => this.props.navigation.navigate('Settings')} style={styles.settingsContainer}>
-            <Image source={require('../assets/images/settings.png')} style={styles.settings} />
-          </TouchableOpacity>
+          <RobotText style={styles.text} text={this.state.text} />
         </View>
       </View>
     );
   }
 }
+
+/*<TouchableOpacity onPress={() => this.props.navigation.navigate('Settings')} style={styles.settingsContainer}>
+  <Image source={require('../assets/images/settings.png')} style={styles.settings} />
+</TouchableOpacity>*/
 
 const styles = StyleSheet.create({
   container: {
@@ -162,14 +235,14 @@ const styles = StyleSheet.create({
     right: 0,
     bottom: 0,
     alignItems: 'center',
+    justifyContent: 'space-around',
   },
   backgroundImage: {
     flex: 1,
     resizeMode: 'cover',
   },
   logo: {
-    marginTop: 60,
-    marginBottom: 80,
+    paddingTop: 100,
   },
   settingsContainer: {
     position: 'absolute',
@@ -181,8 +254,11 @@ const styles = StyleSheet.create({
     height: 60,
   },
   text: {
+    marginBottom: 100,
     fontFamily: 'rationale',
     color: '#ccc',
-    fontSize: 30,
+    fontSize: 35,
+    paddingLeft: 20,
+    paddingRight: 20,
   },
 });
